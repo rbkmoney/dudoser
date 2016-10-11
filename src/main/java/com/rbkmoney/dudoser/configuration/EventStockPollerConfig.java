1e06f47e0c90790a10fe1ec8bb9177d37a523060
avcherkasov <shnurok14@ya.ru>
2016-10-11 15:01:12 +0300

    DUD-1: optimize imports

--- src/main/java/com/rbkmoney/dudoser/configuration/EventStockPollerConfig.java
+++ src/main/java/com/rbkmoney/dudoser/configuration/EventStockPollerConfig.java
@@ -13,11 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.util.List;
-
-import static com.sun.jmx.snmp.EnumRowStatus.destroy;
 
 @Configuration
 public class EventStockPollerConfig {
