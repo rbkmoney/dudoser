ae73bf43b47138a3bdd56c462804a366b0394f1f
avcherkasov <shnurok14@ya.ru>
2016-10-12 13:44:01 +0300

    DUD-1: optimize longToBigDecimal

--- src/main/java/com/rbkmoney/dudoser/utils/Converter.java
+++ src/main/java/com/rbkmoney/dudoser/utils/Converter.java
@@ -1,24 +1,11 @@
 package com.rbkmoney.dudoser.utils;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 
 public class Converter {
 
     public static BigDecimal longToBigDecimal(long amount) {
-        DecimalFormat currencyFormat = new DecimalFormat();
-
-        DecimalFormatSymbols dfs = currencyFormat.getDecimalFormatSymbols();
-        dfs.setDecimalSeparator('.');
-        currencyFormat.setDecimalFormatSymbols(dfs);
-
-        currencyFormat.setMinimumFractionDigits(2);
-        currencyFormat.setMaximumFractionDigits(2);
-        currencyFormat.setGroupingUsed(false);
-
-        double roubles = (double) amount / 100;
-        return new BigDecimal(currencyFormat.format(roubles));
+        return new BigDecimal(amount).divide(BigDecimal.valueOf(100)).setScale(2);
     }
 
     public static Long stringToLong(String str) throws NumberFormatException {
