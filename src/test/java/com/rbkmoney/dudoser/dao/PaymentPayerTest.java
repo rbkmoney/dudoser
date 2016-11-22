79c5ef80fc1b7ad11659e5a9f44ee9f76dc51f3d
Inal Arsanukaev <in.all.young@gmail.com>
2016-11-22 21:08:47 +0300

    Fix some minor bugs-4

--- src/test/java/com/rbkmoney/dudoser/dao/PaymentPayerTest.java
+++ src/test/java/com/rbkmoney/dudoser/dao/PaymentPayerTest.java
@@ -19,19 +19,19 @@ public class PaymentPayerTest {
     public void testGetAmountWithCurrency() throws Exception {
         paymentPayer.setAmount(Converter.longToBigDecimal(12345L));
         paymentPayer.setCurrency("RUB");
-        assertEquals("123,45 RUB", paymentPayer.getAmountWithCurrency());
+        assertEquals("123.45 RUB", paymentPayer.getAmountWithCurrency());
 
         paymentPayer.setAmount(Converter.longToBigDecimal(12145345L));
         paymentPayer.setCurrency("USD");
-        assertEquals("121453,45 USD", paymentPayer.getAmountWithCurrency());
+        assertEquals("121453.45 USD", paymentPayer.getAmountWithCurrency());
 
         paymentPayer.setAmount(Converter.longToBigDecimal(0L));
         paymentPayer.setCurrency("BYR");
-        assertEquals("0,00 BYR", paymentPayer.getAmountWithCurrency());
+        assertEquals("0.00 BYR", paymentPayer.getAmountWithCurrency());
 
         paymentPayer.setAmount(Converter.longToBigDecimal(-1L));
         paymentPayer.setCurrency("FRA");
-        assertEquals("-0,01 FRA", paymentPayer.getAmountWithCurrency());
+        assertEquals("-0.01 FRA", paymentPayer.getAmountWithCurrency());
     }
 
 //    @Test
