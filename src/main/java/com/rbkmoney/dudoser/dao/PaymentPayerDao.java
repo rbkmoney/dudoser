0c109825cb56fd0308f5a010e2f32c1dfaa39a4b
Inal Arsanukaev <in.all.young@gmail.com>
2017-08-17 20:28:31 +0300

    Ft/dud 30/new mail template (#32)
    * DUD-24: Added timeout for mail server
    
    * management enabled fix
    
    * DUD-30: New template
    
    * fix pom.xml
    
    * removed main method
    
    * fixed error logging

--- src/main/java/com/rbkmoney/dudoser/dao/PaymentPayerDao.java
+++ src/main/java/com/rbkmoney/dudoser/dao/PaymentPayerDao.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 public interface PaymentPayerDao {
     Optional<PaymentPayer> getById(String id) throws Exception;
 
-    boolean add(PaymentPayer customer) throws Exception;
+    boolean update(PaymentPayer customer) throws Exception;
+
+    boolean add(String invoiceId, String partyId, String shopId, String shopUrl);
 
     boolean delete(String id) throws Exception;
 }
