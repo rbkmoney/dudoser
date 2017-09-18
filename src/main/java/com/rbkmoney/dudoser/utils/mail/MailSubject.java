121003987e1b43e7207d595401a96c1c8a3c83db
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2017-09-18 17:06:14 +0300

    Added mail subject for refunds

--- src/main/java/com/rbkmoney/dudoser/utils/mail/MailSubject.java
+++ src/main/java/com/rbkmoney/dudoser/utils/mail/MailSubject.java
@@ -18,7 +18,16 @@ public enum MailSubject {
      * Example:
      * String.format(MailSubject.PAYMENT_PAID.pattern, invoiceId, date, amount);
      */
-    PAYMENT_PAID("Счет № %s от %s на сумму %s. Успешно оплачен");
+    PAYMENT_PAID("Счет № %s от %s на сумму %s. Успешно оплачен"),
+
+    /**
+     * Fields: InvoiceId, Date, Amount
+     * Возврат средств на счет № Dud124 от 12-12-2016 на сумму 100.12 RUB.
+     * <p>
+     * Example:
+     * String.format(MailSubject.REFUNDED.pattern, invoiceId, date, amount);
+     */
+    REFUNDED("Возврат средств на счет № %s от %s на сумму %s.");
 
     MailSubject(String pattern) {
         this.pattern = pattern;
