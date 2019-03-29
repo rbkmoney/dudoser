3ae26e4f78cb5960244da806eb3468a437748c9d
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2019-03-29 18:19:48 +0300

    Added records in last_event_id

--- src/main/resources/db/migration/V12__parallel_polling.sql
+++ src/main/resources/db/migration/V12__parallel_polling.sql
@@ -1,1 +1,5 @@
 update dudos.last_event_id set id = 0;
+insert into dudos.last_event_id(id, event_id) values (1, (select event_id from dudos.last_event_id where id = 0));
+insert into dudos.last_event_id(id, event_id) values (2, (select event_id from dudos.last_event_id where id = 0));
+insert into dudos.last_event_id(id, event_id) values (3, (select event_id from dudos.last_event_id where id = 0));
+insert into dudos.last_event_id(id, event_id) values (4, (select event_id from dudos.last_event_id where id = 0));
