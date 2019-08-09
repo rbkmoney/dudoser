7d9dbb834202504dc91192984f62f5e0a9607518
vbanin <vitaxa93gamebox@gmal.com>
2019-08-09 16:03:31 +0300

    test fix

--- src/test/java/com/rbkmoney/dudoser/dao/PaymentPayerTest.java
+++ src/test/java/com/rbkmoney/dudoser/dao/PaymentPayerTest.java
@@ -1,15 +1,9 @@
 package com.rbkmoney.dudoser.dao;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rbkmoney.dudoser.TestData;
-import com.rbkmoney.dudoser.dao.model.Content;
 import com.rbkmoney.dudoser.dao.model.PaymentPayer;
-import com.rbkmoney.dudoser.service.template.model.KebMetadata;
 import com.rbkmoney.dudoser.utils.Converter;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -39,20 +33,5 @@ public class PaymentPayerTest {
         paymentPayer.setAmount(Converter.longToBigDecimal(-1L));
         paymentPayer.setCurrency("FRA");
         assertEquals("-0.01 FRA", Converter.getFormattedAmount(paymentPayer.getAmount(), paymentPayer.getCurrency()));
-    }
-
-    @Test
-    public void testKebParseMetadata() throws IOException {
-        Content content = new Content();
-        content.setType("test");
-        content.setData(TestData.kebMetadata());
-        KebMetadata kebMetadata = new ObjectMapper().readValue(content.getDataValue(), KebMetadata.class);
-        assertEquals(kebMetadata.getTxnId(), "12345678901234567890");
-        assertEquals((int) kebMetadata.getCheckResultCode(), 1);
-        assertEquals(kebMetadata.getCustomerInitials(), "Говнов Петр Сергеевич");
-        assertEquals(kebMetadata.getCommission(), true);
-        assertEquals(kebMetadata.getAccount(), "40817810059900010245");
-        assertEquals(kebMetadata.getSum().toString(), "10000.00");
-        assertEquals(kebMetadata.getCommissionAmount().toString(), "0.01");
     }
 }
